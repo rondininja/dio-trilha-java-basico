845ca9383c7f6bf992d4c43160a314acee343f89
rondininja <phrondini@gmail.com>
2025-03-04 19:00:46 -0300

    Anotações das aulas sobre variáveis e operadores.

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,36 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        
+        boolean variavel = true;
+
+        System.out.println(!variavel);
+        System.out.println(variavel);
+
+        variavel = !variavel;
+        System.out.println(variavel);
+
+
+        /*
+        int numero = 5;
+        x repetição
+
+        numero++;
+        numero = numero + 1
+        System.out.println(numero ++); // se colocar (++ numero), ele ja muda no proprio print.
+
+        System.out.println(numero); 
+        
+        ---------------------------------------------------------------------------------------
+        
+        int numero = 5;
+        numero = -numero;
+        System.out.println(numero);
+
+        numero = numero * -1;
+        System.out.println(numero); 
+        
+        */
+        
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,33 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // Casting = ints não podem ser transformados em short, então é necessário adicionar um filtro para prosseguir mesmo assim.
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        // final é para valores constantes
+        final double VALOR_DE_PI = 3.14;
+
     }
+
+    /* 
+
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; ---- se começar com zero, pode ser necessário usar outro tipo
+        long cpf = 98765432109L; ---- se começar com zero, pode ser necessário usar outro tipo
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        long precisa terminar com L e float precisa terminar com F 
+        
+        */
 }
