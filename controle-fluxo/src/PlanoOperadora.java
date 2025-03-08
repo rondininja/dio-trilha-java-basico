6faaaf1acc58b8a92de5a66a1d74938d9da38e85
rondininja <phrondini@gmail.com>
2025-03-08 13:19:53 -0300

    Anotações de Introdução para Estruturas Condicionais com Java

---
+++ controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,31 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+
+        String plano = "M";
+
+        switch (plano){
+            case "T": {
+                System.out.println("5GB Youtube");
+            }
+            case "M": {
+                System.out.println("Whatsapp e Instagram grátis");
+            }
+            case "B": {
+                System.out.println("100 minutos de ligação");
+            }
+        }
+
+
+        // if (plano == "B") {
+        //     System.out.println("100 minutos de ligação");
+        // } else if (plano == "M") {
+        //     System.out.println("100 minutos de ligação");
+        //     System.out.println("Whatsapp e Instagram grátis");
+        // } else if (plano == "T") {
+        //     System.out.println("100 minutos de ligação");
+        //     System.out.println("Whatsapp e Instagram grátis");
+        //     System.out.println("5GB para Youtube");
+        // }
+
+    }
+}
